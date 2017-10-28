f597a519b04d4230579c894857a988e8fa26488c
CheNyibe <n01241014@ospreys.unf.edu>
2017-10-28 13:17:47 -0400

    Update OPTAB.java
    not completed, token class not available yet

--- OPTAB.java
+++ OPTAB.java
@@ -1,7 +1,7 @@
 import java.util.*;
-public class OPTAB
+public class OPTAB extends Token ();
 {
-   private static class OPTAB_ENTRY
+   private static class 
    {
       public Token.Value val;
       //Token used as key to check each element of the OPTAB
@@ -20,17 +20,17 @@ public class OPTAB
    {
       optab = new HashMap<String, OPTAB_ENTRY>();
       //Creates new entries for the elements of the hashTable ie the OPTAB from Appendix
-      optab.put("ADD", new OPTAB_ENTRY(Token.Value.OP0, 18));
-      optab.put("ADDF", new OPTAB_ENTRY(Token.Value.OP0, 58));
-      optab.put("ADDR", new OPTAB_ENTRY(Token.Value.OP0, 90));
-      optab.put("AND", new OPTAB_ENTRY(Token.Value.OP0, 40));
-      optab.put("CLEAR", new OPTAB_ENTRY(Token.Value.CLEAR, B4));
-      optab.put("COMP", new OPTAB_ENTRY(Token.Value.OP0, 28));
-      optab.put("COMPF", new OPTAB_ENRTY(Token.Value.OP0, 88));
-      optab.put("COMPR", new OPTAB_ENTRY(Token.Value.OP0, A0));
-      optab.put("DIV", new OPTAB_ENTRTY(Token.Value.OP0, 24));
-      optab.put("DIVF", new OPTAB_ENTRY(Token.Value.OP0, 64));
-      optab.put("DIVR", new OPTAB_ENTRY(Token.Value.OP0, 9C)); 
+      optab.put("ADD", new OPTAB_ENTRY(Token.Value.OP0, 0x18));
+      optab.put("ADDF", new OPTAB_ENTRY(Token.Value.OP0, 0x58));
+      optab.put("ADDR", new OPTAB_ENTRY(Token.Value.OP0, 0x90));
+      optab.put("AND", new OPTAB_ENTRY(Token.Value.OP0, 0x40));
+      optab.put("CLEAR", new OPTAB_ENTRY(Token.Value.CLEAR, 0xB4));
+      optab.put("COMP", new OPTAB_ENTRY(Token.Value.OP0, 0x28));
+      optab.put("COMPF", new OPTAB_ENRTY(Token.Value.OP0, 0x88));
+      optab.put("COMPR", new OPTAB_ENTRY(Token.Value.OP0, 0xA0));
+      optab.put("DIV", new OPTAB_ENTRTY(Token.Value.OP0, 0x24));
+      optab.put("DIVF", new OPTAB_ENTRY(Token.Value.OP0, 0x64));
+      optab.put("DIVR", new OPTAB_ENTRY(Token.Value.OP0, 0x9C)); 
        
    }
 }
